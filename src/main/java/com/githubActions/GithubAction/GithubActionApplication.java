0a471dfa0ff6c4a89dbc5658c44b2c3a3fd7f4d4
pawan18github <pawanofficial18@gmail.com>
2024-09-22 13:50:55 +0530

    all

---
+++ src/main/java/com/githubActions/GithubAction/GithubActionApplication.java
@@ -0,0 +1,14 @@
+package com.githubActions.GithubAction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionApplication.class, args);
+		System.out.println("Hello World !!");
+	}
+
+}
