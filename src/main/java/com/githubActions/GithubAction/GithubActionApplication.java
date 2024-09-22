0985af6546d013693fd120ec79140e7351ac0ba4
pawan18github <pawanofficial18@gmail.com>
2024-09-22 15:48:57 +0530

    war

--- src/main/java/com/githubActions/GithubAction/GithubActionApplication.java
+++ src/main/java/com/githubActions/GithubAction/GithubActionApplication.java
@@ -2,13 +2,21 @@ package com.githubActions.GithubAction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GithubActionApplication {
+public class GithubActionApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(GithubActionApplication.class, args);
 		System.out.println("Hello World !!");
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(GithubActionApplication.class);
 	}
 
 }
