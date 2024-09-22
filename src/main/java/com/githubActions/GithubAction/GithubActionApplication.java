c63b2a72494069953a895a0772972ebd7b9243ca
pawan18github <pawanofficial18@gmail.com>
2024-09-22 16:02:04 +0530

    happy api

--- src/main/java/com/githubActions/GithubAction/GithubActionApplication.java
+++ src/main/java/com/githubActions/GithubAction/GithubActionApplication.java
@@ -4,10 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GithubActionApplication extends SpringBootServletInitializer{
 
+	@GetMapping("/happy")
+	public String happy() {
+		return "Happinesss";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GithubActionApplication.class, args);
 		System.out.println("Hello World !!");
